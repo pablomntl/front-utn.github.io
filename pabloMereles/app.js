7818bf9385c5c116345ecc921f54a0f6781cbed8
pablo mereles <pablomereles62@gmail.com>
2021-08-14 18:17:03 -0300

    realice todas las actividades de nuevo

---
+++ pabloMereles/app.js
@@ -0,0 +1,88 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+require('dotenv').config();
+
+var session = require('express-session');
+// var fileUpload = require('express-fileupload');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
+
+var productosRouter = require('./routes/productos')
+var nosotrosRouter = require('./routes/nosotros');
+var contactoRouter = require('./routes/contacto');
+
+
+var app = express();
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'lkpojnvbcsftiqweppzxad',
+  resave: false,
+  saveUninitialized:true
+}));
+
+// app.use(fileUpload({
+//   useTempFiles: true,
+//   tempFileDir: '/tmp/'
+// }));
+
+secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect('/admin/login')
+    }
+  } catch (error){
+    console.log(error);
+  }
+}
+
+
+
+/*rutas*/
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
+
+app.use('/productos', productosRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/contacto', contactoRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
